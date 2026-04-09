fa6f866f02a1d65e5985573d2bd5e196bb57631d
Kimikari <kimiwannaplay@gmail.com>
2026-04-09 07:47:38 -0500

    Initial site filess

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/God-simulator/', // change if needed
+  base: '/god-simulator/', // change if needed
 })
